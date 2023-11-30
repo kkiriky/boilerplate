823687e750acee7e467289d854c3f6915a005cec
devkkiri <yoon_1139@naver.com>
2023-12-01 01:48:54 +0900

    Feat: Initial settings

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const px0_10 = { ...[...Array(11)].map((_, i) => `${i}px`) };
+const px0_100 = { ...[...Array(101)].map((_, i) => `${i}px`) };
+const px0_600 = { ...[...Array(601)].map((_, i) => `${i}px`) };
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      borderWidth: px0_10,
+      borderRadius: px0_100,
+      fontSize: px0_100,
+      lineHeight: px0_100,
+      minWidth: px0_600,
+      minHeight: px0_600,
+      spacing: px0_600,
+    },
+  },
+  corePlugins: {
+    textOpacity: false,
+    backgroundOpacity: false,
+    borderOpacity: false,
+    divideOpacity: false,
+    placeholderOpacity: false,
+    ringOpacity: false,
+  },
+  plugins: [],
+};
