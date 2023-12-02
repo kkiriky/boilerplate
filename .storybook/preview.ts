36abfd8aaf9376c93997ea6ec21e70c45f7079e6
devkkiri <yoon_1139@naver.com>
2023-12-02 17:03:24 +0900

    Refactor: styles

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,10 @@
 import type { Preview } from '@storybook/react';
 import '../src/styles/index.css';
 import '../src/styles/animations.css';
+import '../src/styles/radio.scss';
+import '../src/styles/scrollbar.scss';
 import '../src/styles/react-select.scss';
+import '../src/styles/pagination.scss';
 
 const preview: Preview = {
   parameters: {
