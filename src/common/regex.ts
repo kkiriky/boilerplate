d1f248af07aab6fbe51abe0b72a792f74af0aab3
kkiriky <yoon_1139@naver.com>
2024-03-31 13:00:25 +0900

    refactor: 정규표현식 정리

--- src/common/regex.ts
+++ src/common/regex.ts
@@ -1,8 +1,7 @@
 // 마크다운 이미지 정규표현식
 export const base64ImageRegexForMarkdown =
   /!\[[^\]]*\]\((data:image\/[^;]+;base64,.*?)\)/g;
-// export const base64ImageDataRegex = /data:image\/[^;]+;base64,([^')"]+)/g;
 
-// export const base64ImageRegexForHtml = /<img.*?src="(data:image\/[^;]+;base64,([^"]+))"[^>]*>/
+// HTML 이미지 태그 정규표현식
 export const base64ImageRegexForHtml =
   /<img.*?src="(data:image\/[^;]+;base64,[^"]+)"[^>]*>/g;
